eea5dd4a320876359ff1d217d8143b99efa593d3
benbryson789 <deborashelby@gmail.com>
2021-01-01 21:39:32 -0500

    first commit

---
+++ Switch Example C Sharp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Switch_Example_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             //switch without combining expressions
+
+            var season = "a";
+
+            switch (season)
+            {
+                case "autumn":
+                    Console.WriteLine("We've  got a promotion");
+                    break;
+                case "summer":
+                    Console.WriteLine("We've got a promotion");
+                    break;
+                default:
+                    Console.WriteLine("We do not have a promotion");
+                    break;
+            }
+
+            */
+            
+
+            /*
+             
+            //switch statement that combines expressions
+
+            var season = "fall";
+
+            switch (season)
+            {
+                case "autumn":
+                    
+                case "summer":
+                    Console.WriteLine("We've got a promotion");
+                    break;
+                default:
+                    Console.WriteLine("We do not have a promotion");
+                    break;
+            
+            
+            }
+            */
+        }
+    }
+}
